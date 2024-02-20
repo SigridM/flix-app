310c8c1ebd713942548cd38a78c415061791a8fa
SigridM <sigridm@selfhelpsoftware.com>
2024-02-20 15:01:05 -0500

    Moved overlay style into style sheet

--- js/script.js
+++ js/script.js
@@ -84,16 +84,17 @@ const currencyFormatter = new Intl.NumberFormat('en-US', {
 function displayBackgroundImage(path, isTV = false) {
   const overlayDiv = document.createElement('div');
   overlayDiv.style.backgroundImage = `url(https:/image.tmdb.org/t/p/original/${path})`; // 'original' meand original size
-  overlayDiv.style.backgroundSize = 'cover';
-  overlayDiv.style.backgroundPosition = 'center';
-  overlayDiv.style.backgroundRepeat = 'no-repeat';
-  overlayDiv.style.height = '100vh';
-  overlayDiv.style.width = '100vw';
-  overlayDiv.style.position = 'absolute';
-  overlayDiv.style.top = '0';
-  overlayDiv.style.left = '0';
-  overlayDiv.style.zIndex = '-1';
-  overlayDiv.style.opacity = '0.15';
+  //   overlayDiv.style.backgroundSize = 'cover';
+  //   overlayDiv.style.backgroundPosition = 'center';
+  //   overlayDiv.style.backgroundRepeat = 'no-repeat';
+  //   overlayDiv.style.height = '100vh';
+  //   overlayDiv.style.width = '100vw';
+  //   overlayDiv.style.position = 'absolute';
+  //   overlayDiv.style.top = '0';
+  //   overlayDiv.style.left = '0';
+  //   overlayDiv.style.zIndex = '-1';
+  //   overlayDiv.style.opacity = '0.15';
+  overlayDiv.classList.add('overlay');
 
   if (isTV) {
     document.querySelector('#tv-details').appendChild(overlayDiv);
