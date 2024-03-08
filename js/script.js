2e75b3274d96d97e93f747305c11ec19c7e02381
SigridM <sigridm@selfhelpsoftware.com>
2024-03-08 12:59:47 -0500

    Added keyword vs. title search to search panel

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ import { global } from './globals.js';
 import { displaySlider } from './imageManagement.js';
 import { displayDetails, displayPopular } from './commonElements.js';
 import { search } from './search.js';
+import { addFilterListeners } from './filter.js';
 
 // Highlight active link
 function highlightActiveLink() {
@@ -14,16 +15,18 @@ function highlightActiveLink() {
 }
 
 // Init App - runs on every page
-async function init() {
+function init() {
   switch (
     global.currentPage // Simple router
   ) {
     case '/':
     case '/index.html':
+      initSearchForm();
       displaySlider();
       displayPopular();
       break;
     case '/shows.html':
+      initSearchForm();
       displaySlider(true);
       displayPopular(true);
       break;
@@ -34,10 +37,20 @@ async function init() {
       displayDetails(true);
       break;
     case '/search.html':
+      initSearchForm();
       search();
       break;
   }
   highlightActiveLink();
+}
+
+function initSearchForm() {
+  const searchForm = document.querySelector('#search-form');
+  searchForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+    search();
+  });
+  addFilterListeners(global.search.type == 'tv');
 }
 
 document.addEventListener('DOMContentLoaded', init);
