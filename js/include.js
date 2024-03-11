dc5929f378db005921ea2715649dfba55bbf758b
SigridM <sigridm@selfhelpsoftware.com>
2024-03-11 13:22:19 -0400

    remove console logs

--- js/include.js
+++ js/include.js
@@ -1,10 +1,7 @@
 function includeHTML(path, elementID, alsoDo = null) {
-  console.log('in includeHTML', elementID);
-
   fetch(path)
     .then((response) => response.text())
     .then((html) => {
-      console.log(document.getElementById(elementID));
       document.getElementById(elementID).innerHTML = html;
       if (alsoDo) {
         alsoDo();
