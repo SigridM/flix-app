0a66ef6267479aa11a7369d8b36e6c44dabd9e85
SigridM <sigridm@selfhelpsoftware.com>
2024-04-02 11:56:26 -0400

    Fixed comment

--- js/commonElements.js
+++ js/commonElements.js
@@ -286,7 +286,7 @@ function addStatus(media, details) {
   });
 }
 
-// Add the status to the array of details, if that detail exists
+// Add the adult Boolean to the array of details
 function addAdult(media, details) {
   let detail = media.adult;
   details.push({
